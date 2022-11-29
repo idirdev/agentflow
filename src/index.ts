8d9d8856b51f80f4831a15e4ca53773cfd2394e4
idirdev <idirdev@users.noreply.github.com>
2022-11-29 03:24:28 +0100

    fix: clean up unused deps and add missing re-exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,18 @@ export { WorkflowEngine } from "./core/engine.js";
 export { Agent } from "./core/agent.js";
 export { Tool } from "./core/tool.js";
 export { PromptTemplate } from "./core/prompt.js";
+export { LRUCache } from "./core/cache.js";
+export { CostManager } from "./core/cost.js";
+export { HookManager } from "./core/hooks.js";
+export type { BeforeStepHook, AfterStepHook, OnErrorHook, OnCompleteHook } from "./core/hooks.js";
+export { RetryHandler } from "./core/retry.js";
+export type { RetryOptions } from "./core/retry.js";
 export { MemoryStore } from "./memory/store.js";
 export { ModelRouter } from "./models/router.js";
+export { Logger } from "./utils/logger.js";
+export { OutputParser } from "./utils/parser.js";
+export { WorkflowValidator } from "./utils/validation.js";
+export type { ValidationResult } from "./utils/validation.js";
 
 export type {
   AgentConfig,
@@ -11,6 +21,11 @@ export type {
   StepConfig,
   ToolDefinition,
   WorkflowResult,
+  StepResult,
   AgentMessage,
   ModelProvider,
+  RetryConfig,
+  ParameterDef,
+  CostTracker,
+  MemoryEntry,
 } from "./types.js";
